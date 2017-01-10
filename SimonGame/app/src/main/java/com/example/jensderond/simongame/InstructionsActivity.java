4bc6b2bf99aa912f24bec44b83ff66c93de1abc5
Jens de Rond <jensderond@gmail.com>
2017-01-10 13:17:49 +0100

    Adds instructions view and classes
    Also added text strings and text dimensions

---
+++ SimonGame/app/src/main/java/com/example/jensderond/simongame/InstructionsActivity.java
@@ -0,0 +1,17 @@
+package com.example.jensderond.simongame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jensderond on 10/01/2017.
+ */
+
+public class InstructionsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions);
+    }
+}
